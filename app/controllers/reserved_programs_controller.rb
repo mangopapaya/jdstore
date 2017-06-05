0a630ec0d3255afc3482bc9c2ce9625f8c104aeb
Yoanna <huang.yoanna@gmail.com>
2017-06-05 20:10:02 +0800

    cannot reserve program exceeding available space

--- app/controllers/reserved_programs_controller.rb
+++ app/controllers/reserved_programs_controller.rb
@@ -14,7 +14,12 @@ class ReservedProgramsController < ApplicationController
   def update
     @reservation = current_reservation
     @reserved_program = @reservation.reserved_programs.find_by(program_id: params[:id])
-    @reserved_program.update (reserved_program_params)
+      if  @reserved_program.program.space >= reserved_program_params[:slot].to_i
+        @reserved_program.update (reserved_program_params)
+        flash[:notice] = "Reservation updated"
+      else
+        flash[:warning] = "Oops, not enough space for this program."
+      end
     redirect_to reservations_path
   end
 
