adac0a4a8d34ec19071cb97231c3f1ca849d94ca
Yoanna <huang.yoanna@gmail.com>
2017-06-18 14:17:13 +0800

    added revised model for admin/category

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,57 @@
+class Admin::CategoriesController < ApplicationController
+
+  layout "admin"
+
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path, warning:"Category deleted"
+  end
+
+  private
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def category_params
+      params.require(:category).permit(:name, :description)
+    end
+
+end
